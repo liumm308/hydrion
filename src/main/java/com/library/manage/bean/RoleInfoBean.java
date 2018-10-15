e6052a4bb38ed576dca5719962c48509a559ba19
liumingming <xidian_liu@163.com>
2018-10-15 17:07:54 +0800

    userRole and redis

---
+++ src/main/java/com/library/manage/bean/RoleInfoBean.java
@@ -0,0 +1,62 @@
+package com.library.manage.bean;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class RoleInfoBean implements Serializable {
+
+    private Integer id;
+    private String roleId;
+    private String roleName;
+    private String status;
+    private Timestamp createTime;
+    private Timestamp updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+
+    public Timestamp getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Timestamp updateTime) {
+        this.updateTime = updateTime;
+    }
+}
---
+++ src/main/java/com/library/manage/bean/UserRoleBean.java
@@ -0,0 +1,61 @@
+package com.library.manage.bean;
+
+import java.sql.Timestamp;
+
+public class UserRoleBean {
+
+    private int id;
+    private String uid;
+    private String rid;
+    private String status;
+    private Timestamp createTime;
+    private Timestamp updateTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getRid() {
+        return rid;
+    }
+
+    public void setRid(String rid) {
+        this.rid = rid;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+
+    public Timestamp getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Timestamp updateTime) {
+        this.updateTime = updateTime;
+    }
+}
